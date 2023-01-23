cc100910d7e87754d181fcbf867644f1fc636beb
Priyanshu Ahuja <85331994+Priyanshuahuja72@users.noreply.github.com>
2023-01-23 20:55:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your code here
+var count = document.getElementById('count');
+var input = document.getElementById('input');
+function isWord(str) {
+  var alphaNumericFound = false;
+  for (var i = 0; i < str.length; i++) {
+    var code = str.charCodeAt(i);
+    if ((code > 47 && code < 58) || // numeric (0-9)
+        (code > 64 && code < 91) || // upper alpha (A-Z)
+        (code > 96 && code < 123)) { // lower alpha (a-z)
+      alphaNumericFound = true;
+      return alphaNumericFound;
+    }
+  }
+  return alphaNumericFound;
+}
+function wordCounter(text) {
+  var text = input.value.split(' ');
+  var wordCount = 0;
+  for (var i = 0; i < text.length; i++) {
+    if (text[i] !== ' ' && isWord(text[i])) {
+      wordCount++;
+    }
+  }
+  count.innerText = wordCount;
+}
+input.addEventListener('keyup' , function(e){
+	 wordCounter(e.target.value);
+});
